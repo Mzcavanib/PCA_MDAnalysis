0d99a8e37e1d8038387b70b39614d92b23013779
Maurizio Cavani <97117734+Mzcavanib@users.noreply.github.com>
2026-03-31 12:44:26 -0500

    Update pca_csv.py

--- pca_csv.py
+++ pca_csv.py
@@ -3,28 +3,22 @@ import MDAnalysis as mda
 from MDAnalysis.analysis import pca
 import pandas as pd
 
-
 u = mda.Universe("final.gro", "final.xtc")
 ca = u.select_atoms("protein and name CA")
 
-
 pca_analysis = pca.PCA(u, select="protein and name CA").run()
-
 
 try:
     p_components = pca_analysis.results.p_components
 except AttributeError:
     p_components = pca_analysis.p_components
 
-
 n_atoms = len(ca)
 pc1 = p_components[:, 0].reshape(n_atoms, 3)
 pc2 = p_components[:, 1].reshape(n_atoms, 3)
 
-
 pc1_magnitudes = np.linalg.norm(pc1, axis=1)
 pc2_magnitudes = np.linalg.norm(pc2, axis=1)
-
 
 resids = ca.resids
 resnames = ca.resnames
@@ -35,7 +29,6 @@ df = pd.DataFrame({
     "PC2_contrib": pc2_magnitudes
 })
 
-# 7. Guardar como CSV
 df.to_csv("residue_contributions.csv", index=False)
 print("Archivo 'residue_contributions.csv' generado.")
 
